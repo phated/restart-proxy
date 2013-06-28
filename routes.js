6050b0d52e56f698d093dddf5966ae78baf415cb
Blaine Bublitz <blaine@iceddev.com>
2013-06-27 18:12:31 -0700

    change route port

--- routes.js
+++ routes.js
@@ -5,5 +5,5 @@ module.exports = {
   'www.hackphx.org': '127.0.0.1:3001',
   'dev.hackphx.com': '127.0.0.1:3001',
   'dev.hackphx.org': '127.0.0.1:3001',
-  'payments.frozenjs.com': '127.0.0.1:9000'
+  'payments.frozenjs.com': '127.0.0.1:9999'
 };
