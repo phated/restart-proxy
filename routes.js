e173cadb098812ebfb75ec47cd74db8d49294bb1
Blaine Bublitz <blaine@iceddev.com>
2013-06-27 17:44:43 -0700

    frozen payments route

--- routes.js
+++ routes.js
@@ -4,5 +4,6 @@ module.exports = {
   'hackphx.org': '127.0.0.1:3001',
   'www.hackphx.org': '127.0.0.1:3001',
   'dev.hackphx.com': '127.0.0.1:3001',
-  'dev.hackphx.org': '127.0.0.1:3001'
+  'dev.hackphx.org': '127.0.0.1:3001',
+  'payments.frozenjs.com': '127.0.0.1:9000'
 };
